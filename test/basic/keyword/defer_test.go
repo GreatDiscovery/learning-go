fbc75cb7b1e40828bcc15603350378bf0bea9493
songbowen <1303535630@qq.com>
2023-06-06 00:02:55 +0800

    add: make keyword

--- test/basic/keyword/defer_test.go
+++ test/basic/keyword/defer_test.go
@@ -1,4 +1,4 @@
-package main
+package keyword
 
 import (
 	"testing"
---
+++ test/basic/keyword/make_test.go
@@ -0,0 +1,17 @@
+package keyword
+
+import "testing"
+
+// what is the difference between make and new keyword.
+// https://draveness.me/golang/docs/part2-foundation/ch05-keyword/golang-make-and-new/
+
+func TestMake(t *testing.T) {
+	//the make keyword works with three types
+	//slice := make([]int, 0, 100)
+	//hash := make(map[int]bool, 10)
+	//ch := make(chan int, 5)
+}
+
+func main() {
+
+}
