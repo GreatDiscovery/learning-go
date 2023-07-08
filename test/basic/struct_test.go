2d5143c7ce8f7db7ce2160586054e8efbf7016f0
GreatDiscovery <1303535630@qq.com>
2023-07-08 16:01:24 +0800

    add: struct tag
    learn from https://juejin.cn/post/7005465902804123679

--- test/basic/struct_test.go
+++ test/basic/struct_test.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 )
 
 type Books struct {
-	title   string
-	author  string
-	subject string
-	bookId  int
+	Title   string `json:"title,omitempty"`
+	Author  string `json:"author,omitempty"`
+	Subject string `json:"subject,omitempty"`
+	BookId  int    `json:"book_id,omitempty"`
 }
 
 func TestStruct(t *testing.T) {
@@ -18,8 +19,23 @@ func TestStruct(t *testing.T) {
 	fmt.Println(Books{"Go 语言", "www.runoob.com", "Go 语言教程", 6495407})
 
 	// 也可以使用 key => value 格式
-	fmt.Println(Books{title: "Go 语言", author: "www.runoob.com", subject: "Go 语言教程", bookId: 6495407})
+	fmt.Println(Books{Title: "Go 语言", Author: "www.runoob.com", Subject: "Go 语言教程", BookId: 6495407})
 
 	// 忽略的字段为 0 或 空
-	fmt.Println(Books{title: "Go 语言", author: "www.runoob.com"})
+	fmt.Println(Books{Title: "Go 语言", Author: "www.runoob.com"})
+}
+
+func TestJson(t *testing.T) {
+	book := Books{
+		Title:   "钢铁是怎样练成的",
+		Author:  "奥斯特洛夫斯基",
+		Subject: "novel",
+		BookId:  1,
+	}
+
+	data, err := json.Marshal(book)
+	if err != nil {
+		return
+	}
+	fmt.Println(string(data))
 }
