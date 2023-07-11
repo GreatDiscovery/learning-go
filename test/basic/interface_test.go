1975680ddd69aad5e97423ede5ebf63ab59325e4
songbowen <1303535630@qq.com>
2023-07-11 23:28:40 +0800

    add: type assertion

--- test/basic/interface_test.go
+++ test/basic/interface_test.go
@@ -31,6 +31,45 @@ func TestInterface(t *testing.T) {
 	phone.call()
 }
 
-func main() {
+//  type assertion，接口断言，就是判断接口是否 是某种类型
+
+type Inter interface {
+	Ping()
+	Pang()
+}
+
+type Anter interface {
+	Inter
+	String()
+}
+
+type St struct {
+	Name string
+}
+
+func (St) Ping() {
+	println("ping")
+}
+
+func (St) Pang() {
+	println("pang")
+}
+
+func TestTypeAssert(t *testing.T) {
+	st := &St{"andes"}
+	var i interface{} = st
+
+	// 判断i绑定的实例是否实现了接口类型Inter
+	o := i.(Inter)
+	o.Ping()
+	o.Pang()
+
+	// i没有实现接口Anter，会报错
+	//p := i.(Anter)
+	//p.String()
+
+	// 判断i实现了具体类型St
+	s := i.(*St)
+	fmt.Printf("%s", s.Name)
 
 }
