78b2b1d08e9c2c4e21de40b89ce892523ec8a573
GreatDiscovery <1303535630@qq.com>
2023-09-14 15:03:25 +0800

    add: chan && select

--- test/concurrent/chan_test.go
+++ test/concurrent/chan_test.go
@@ -7,6 +7,25 @@ import (
 	"time"
 )
 
+func TestSelect(t *testing.T) {
+	ch := make(chan int)
+	go func() {
+		time.Sleep(5 * time.Second)
+		ch <- 5
+	}()
+
+	for {
+		select {
+		case num := <-ch:
+			fmt.Println("num=", num)
+			return
+		default:
+			time.Sleep(1 * time.Second)
+			fmt.Println("if no default, if will block")
+		}
+	}
+}
+
 // 模拟10个协程，同时处理任务，最后收集返回结果
 func TestMultiThread(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
