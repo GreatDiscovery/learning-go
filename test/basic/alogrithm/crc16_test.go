07a23c863f5e54353c641b40277df0202b57c219
GreatDiscovery <1303535630@qq.com>
2023-10-16 15:28:57 +0800

    add: redis slot keys

--- test/basic/alogrithm/crc16_test.go
+++ test/basic/alogrithm/crc16_test.go
@@ -1,12 +1,41 @@
 package alogrithm
 
 import (
+	"fmt"
 	"github.com/joaojeronimo/go-crc16"
+	"strconv"
 	"testing"
+	"time"
 )
 
+// 每个slot 0-16383分配一个key
+func TestGetAllSlotKey(t *testing.T) {
+	slotMap := make(map[int]string)
+	prefix := "slotKey"
+	now := strconv.Itoa(int(time.Now().Unix()))
+
+	start := time.Now()
+	const maxInt = 163850
+
+	for i := 0; i < maxInt; i++ {
+		if len(slotMap) >= 16384 {
+			break
+		}
+		key := prefix + now + "-" + strconv.Itoa(i)
+		slot := GetCRC16(key)
+		if _, ok := slotMap[slot]; !ok {
+			slotMap[slot] = key
+		}
+	}
+
+	end := time.Now()
+	fmt.Println("cost=", end.Sub(start))
+	fmt.Println("slotMap=", slotMap)
+	fmt.Println("slotMap[0]=", slotMap[0])
+}
+
 func TestCrc16(t *testing.T) {
-	println(GetCRC16("_redis_rmt_check_152689"))
+	println(GetCRC16("slotKey1697441090-211"))
 }
 
 // 根据key算出redis的slot
