7f5f4925d217031d894400af0f6ba62487538999
GreatDiscovery <1303535630@qq.com>
2023-10-25 10:30:11 +0800

    add: todo Parallelizer().Until func

---
+++ test/util/parallelism_test.go
@@ -0,0 +1,11 @@
+package util
+
+import (
+	"testing"
+)
+
+// 测试批量并行工具，控制并发和处理异常
+func TestParallel(t *testing.T) {
+	// todo how to use parallelizer
+	//fwk.Parallelizer().Until(ctx, numAllNodes, checkNode, metrics.Filter)
+}
