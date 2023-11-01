4b5c32a813370db303dd7fcfdf9674c8e47f770a
GreatDiscovery <1303535630@qq.com>
2023-11-01 10:52:01 +0800

    update: server
    todo unaryCall

--- test/framework/rpc_server/services.go
+++ test/framework/rpc_server/services.go
@@ -6,6 +6,8 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"learning-go/test/framework/rpc_server/rpc_server"
+	"path"
+	"time"
 )
 
 type StreamServer interface {
@@ -37,11 +39,56 @@ func (s *serviceSet) register(name string, desc *ServiceDesc) {
 	s.services[name] = desc
 }
 
-func (s *serviceSet) handler(ctx context.Context, req rpc_server.Request, response func(*status.Status, []byte, bool, bool) error) (*StreamHandler, error) {
-	_, ok := s.services[req.Service]
+func (s *serviceSet) handler(ctx context.Context, req *rpc_server.Request, response func(*status.Status, []byte, bool, bool) error) (*StreamHandler, error) {
+	srv, ok := s.services[req.Service]
 	if !ok {
 		// 使用code来对报错进行快速分类
 		return nil, status.Errorf(codes.Unimplemented, "service %v", req.Service)
 	}
+	if method, ok := srv.Methods[req.Method]; ok {
+		go func() {
+			// 从request那里获取到了meta信息，超时信息
+			ctx, cancel := getRequestContext(ctx, req)
+			defer cancel()
+
+			info := &UnaryServerInfo{fullPath(req.Service, req.Method)}
+			p, st := s.unaryCall(ctx, method, info, req.Payload)
+			response(st, p, false, true)
+		}()
+		return nil, nil
+	}
+	return nil, nil
+}
+
+type MD map[string][]string
+type metadataKey struct{}
+
+var noopFunc = func() {}
+
+func (m MD) fromContext(request *rpc_server.Request) {
+	for _, meta := range request.Metadata {
+		m[meta.Key] = append(m[meta.Key], meta.Value)
+	}
+}
+
+func getRequestContext(ctx context.Context, req *rpc_server.Request) (retCtx context.Context, cancel func()) {
+	if len(req.Metadata) > 0 {
+		md := MD{}
+		md.fromContext(req)
+		ctx = context.WithValue(ctx, metadataKey{}, md)
+	}
+	cancel = noopFunc
+	if req.TimeoutNano == 0 {
+		return ctx, cancel
+	}
+	ctx, cancel = context.WithTimeout(ctx, time.Duration(req.TimeoutNano))
+	return ctx, cancel
+}
+
+func fullPath(service string, method string) string {
+	return "/" + path.Join(service, method)
+}
+
+func (s *serviceSet) unaryCall(ctx context.Context, method Method, info *UnaryServerInfo, data []byte) (p []byte, st *status.Status) {
 	return nil, nil
 }
